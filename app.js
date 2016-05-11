f4b803863675608015fbcf9308dcfaa2a13b7740
Fabricio Ronchi <fabricioronchii@gmail.com>
2016-05-11 18:30:30 -0300

    Remove service folder and add routers

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var express = require('express'),
     path = require('path'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
+    router_urls = require('./routes/urls'),
+    router_users = require('./routes/users'),
+    router_stats = require('./routes/stats'),
     app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/')));
+
+// Router config
+app.use('/urls', router_urls);
+app.use('/users', router_users);
+app.use('/stats', router_stats);
 
 module.exports = app;
---
+++ routes/stats.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express'),
+		router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('Hello Stats');
+});
+
+module.exports = router;
