0c9049d7dffe26df6166c59f4c26fd0d12d50449
Fabricio Ronchi <fabricioronchii@gmail.com>
2016-05-13 22:03:05 -0300

    Add promisses

--- models/urls.js
+++ models/urls.js
@@ -4,9 +4,18 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var Urls = mongoose.model('Urls', new Schema({
+	id: { type: String },
   url: { type: String },
+  shortUrl: { type: String },
   hits: { type: Number },
-  url: { type: String }
+  userId: { type: Schema.Types.ObjectId, ref: 'User' }
+}).pre('save', function(next) {
+	var doc = this;
+  this.constructor.count().exec(function(err, count) {
+	  if(err) return next(err);
+	  doc.id = count++;
+	  next();
+  });
 }));
 
 module.exports = Urls;
--- routes/users.js
+++ routes/users.js
@@ -3,45 +3,60 @@
 var express = require('express'),
 		router = express.Router(),
 		codes = require('../config/codes'),
-		User = require('../models/users');
+		User = require('../models/users'),
+    Urls = require('../models/urls');
 
 router.post('/', function(req, res, next){
   var user = new User();
 	user.id = req.body.id;
-  user.save(function(err, data){
-  	res.status(codes.CONFLICT_CODE);
-  	if(err === null){
-  		res.status(codes.CREATED_CODE);
-  		data = { id: data.id };
-  	};
-  	res.send(data);
-	});
+  var promisse = user.save();
+  promisse.then(function(data) {
+    res.status(codes.CREATED_CODE);
+    res.send({ id: data.id });
+  }).catch(function(err){
+    res.status(codes.CONFLICT_CODE).send();
+  });
 });
 
 router.post('/:userid/urls', function(req, res, next){
-  var user = new User();
-	user.id = req.body.id;
-  user.save(function(err, data){
-  	res.status(codes.CONFLICT_CODE);
-  	if(err === null){
-  		res.status(codes.CREATED_CODE);
-  		data = { id: data.id };
-  	};
-  	res.send(data);
-	});
+  var promisse = User.findOne({
+    id: req.param('userid')
+  }).exec();
+  promisse.then(function(user) {
+    if(user){
+      var urls = new Urls();
+      urls.hits = 0;
+      urls.url = 'www.google.com';
+      urls.shortUrl = 'http://short.url.com/teste';
+      urls.userId = user;
+      return urls.save().then(function(url){
+        return url;
+      });
+    }else{
+      res.status(codes.NOTFOUND_CODE).send();
+    }
+  }).then(function(url) {
+    res.status(codes.CREATED_CODE);
+    res.send({
+      id: url.id,
+      hits: url.hits,
+      url: url.url
+    });
+  }).catch(function(err){
+    res.status(codes.CONFLICT_CODE).send();
+  });
 });
 
 router.delete('/:userId', function(req, res, next){
-  User.remove({
+  var promisse = User.remove({
     id: req.param('userId')
-  }, function(err, data) {
+  }).exec();
+  promisse.then(function(data) {
   	res.status(codes.NOTCONTENT_CODE)
-  	if(err){
-  		res.status(codes.BADREQUEST_CODE);
-  	}else{
-  		if(data.result.n === 0) res.status(codes.NOTFOUND_CODE);
-  	}
+    if(data.result.n === 0) res.status(codes.NOTFOUND_CODE);
   	res.send();
+  }).catch(function(err){
+    res.status(codes.BADREQUEST_CODE).send();
   });
 });
 
